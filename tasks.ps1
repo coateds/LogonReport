529cfcb82fa403cc110b7af3a9c36431c35536c9
Dave <you@example.com>
2017-08-14 11:06:25 -0700

    Add AC Power Settings

--- tasks.ps1
+++ tasks.ps1
@@ -2,10 +2,10 @@
 $T = New-ScheduledTaskTrigger -AtLogon
 # $A = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\Users\dcoate\Documents\GitRepositories\LogonReport\Logon.ps1"
 $A = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "~\Documents\GitRepositories\LogonReport\Logon.ps1"
-$S = New-ScheduledTaskSettingsSet
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 
 # This will set up a scheduled task to run the PowerShell logon script
-Register-ScheduledTask -TaskName LogonScript -Action $A -Trigger $T -RunLevel Highest
+Register-ScheduledTask -TaskName LogonScript -Action $A -Trigger $T -RunLevel Highest -Settings $S
 
 # this will remove the logon script
 Get-ScheduledTask -TaskName Logon* | Unregister-ScheduledTask
